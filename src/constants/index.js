3458c2adf7681b4ec3fa3ecc7ed2f38eef70fb4a
Joseph Reyes <jreyes1919@gmail.com>
2024-03-23 18:58:50 -0500

    finished nav
    finished nav section (more styling to be done later)

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const NavLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#About", label: "About" },
+  { href: "#Services", label: "Services" },
+  { href: "#Contact", label: "Contact" },
+];
